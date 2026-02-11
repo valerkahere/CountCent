0bae29cf4e1e02501c48c4d4934ef08f09ddd751
Valerii Navalnyi <adultvalerka@gmail.com>
2026-02-11 13:48:22 +0000

    Add project files.

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            UserAppTheme = AppTheme.Light;
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
