cec5c2dfd2dc4b70edbd4e7f4be3970b4e4bccd7
Valerii Navalnyi <adultvalerka@gmail.com>
2026-02-12 18:43:15 +0000

    Cleanup

--- CountCent/Model/Entry.cs
+++ CountCent/Model/Entry.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CountCent.ViewModel
+namespace CountCent.Model
 {
-    class MainViewModel
+    public class Entry
     {
     }
 }
