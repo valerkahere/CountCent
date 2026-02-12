a2eb8a70d3a658a368f9d9b15c82b81cfd93c728
Valerii Navalnyi <adultvalerka@gmail.com>
2026-02-12 11:35:42 +0000

    Fix BindingContext for AboutPage

---
+++ CountCent/Models/About.cs
@@ -0,0 +1,12 @@
+﻿namespace CountCent.Models
+{
+    internal class About
+    {
+        // Here Lambda operator means
+        // This is read-only property that returns this value
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://www.valerkahere.com/";
+        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+    }
+}
--- CountCent/Views/AboutPage.xaml.cs
+++ CountCent/Views/AboutPage.xaml.cs
@@ -1,22 +1,30 @@
+using System.Diagnostics;
+using CountCent.Models;
+
 namespace CountCent.Views
 {
     public partial class AboutPage : ContentPage
     {
-
-
-
-
         public AboutPage()
         {
             InitializeComponent();
 
-
+            // Connect the Model to the View manually
+            // so LearnMore_Clicked method works
+            BindingContext = new About();
         }
 
         private async void LearnMore_Clicked(object sender, EventArgs e)
         {
             // Navigate to the specified URL in the system browser
-            await Launcher.Default.OpenAsync("https://valerkahere.com");
+            // what is bindingContext type?
+            Debug.WriteLine(BindingContext.GetType().ToString());
+            Debug.WriteLine(BindingContext.ToString());
+
+            if (BindingContext is Models.About about)
+            {
+                await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+            }
         }
     }
 
