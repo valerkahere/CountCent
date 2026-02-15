4b636e83b47610d7975c8556dd06f9edba3168ab
Valerii Navalnyi <adultvalerka@gmail.com>
2026-02-15 20:58:28 +0000

    Display dummy data

---
+++ CountCent/Model/DataPoint.cs
@@ -0,0 +1,27 @@
+﻿namespace CountCent.Model
+{
+    public class DataPoint
+    {
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public DataPoint()
+        {
+            Amount = 0;
+            Date = DateTime.Now;
+        }
+
+        public DataPoint(decimal amount, DateTime date)
+        {
+            Amount = amount;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            // Alternative with a date
+            // return $"{Amount} - {Date.ToShortDateString()}";
+            return $"{Amount:C1}";
+        }
+    }
+}
--- CountCent/View/MainPage.xaml.cs
+++ CountCent/View/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace CountCent
+﻿using CountCent.Model;
+
+namespace CountCent
 {
     public partial class MainPage : ContentPage
     {
@@ -7,12 +9,29 @@
             InitializeComponent();
         }
 
+
+        // This will store data throughout the program
+        List<DataPoint> dataPoints = new List<DataPoint>();
+
         // behaviour immediately prior to application becoming visible
         protected override void OnAppearing()
         {
             // Get today's date, converts to date, to string (short date), and display it
-            DateTime today = DateTime.Today;
-            lbl_day.Text = today.ToString("d");
+            lbl_day.Text = DateTime.Today.ToString("d");
+
+            // Populate with dummy (or existing) data
+            Random random = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                dataPoints.Add(
+                    new DataPoint(
+                            random.Next(100, 100000),
+                            DateTime.Now
+                        )
+                    );
+            }
+
+            clc__mainScreen.ItemsSource = dataPoints;
         }
 
     }
