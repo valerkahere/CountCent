07537ed5a6f101bc7e43e0a68ccb09c336989cba
Valerii Navalnyi <adultvalerka@gmail.com>
2026-02-15 20:33:40 +0000

    Add current date to the top of the file

--- CountCent/View/MainPage.xaml.cs
+++ CountCent/View/MainPage.xaml.cs
@@ -7,6 +7,13 @@
             InitializeComponent();
         }
 
-        
+        // behaviour immediately prior to application becoming visible
+        protected override void OnAppearing()
+        {
+            // Get today's date, converts to date, to string (short date), and display it
+            DateTime today = DateTime.Today;
+            lbl_day.Text = today.ToString("d");
+        }
+
     }
 }
